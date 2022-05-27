f15ac683ef0a8978d1b3c464ab385261888978bd
Adityo Gizwanda <gizwanda@Adityos-Mac-mini.local>
2022-05-27 19:35:26 +0700

    add new sum function for hitungBunga

---
+++ scripts/calculate.js
@@ -0,0 +1,8 @@
+const sum = require('./common-function')
+
+function hitungBunga(saldo, bunga) {
+    let totalSaldo = sum(saldo, bunga)
+    return totalSaldo
+}
+
+console.log(hitungBunga(100000, 10000))
